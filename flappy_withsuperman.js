85f8de18d1d0055aefbc46920b9a4f3896c1d692
2splashes <dkn22@cam.ac.uk>
2014-11-01 17:28:16 +0000

    Game

--- flappy_withsuperman.js
+++ flappy_withsuperman.js
@@ -14,7 +14,6 @@ score = score + 1;
 var x
 var y
 var player;
-var pipes;
 /*
  * Loads all resources for the game and gives them names.
  */
@@ -22,7 +21,7 @@ function preload() {
     game.load.image("playerImg","assets/jamesBond.gif")
     game.load.image("playah","assets/flappy_superman.png")
     game.load.image("playarr","assets/csep.png")
-    game.load.audio("score","assets/score.mp3")
+    game.load.audio("score","assets/point.ogg")
     game.load.image("player","assets/flappy.png")
     game.load.image("pipe","assets/pipe.png")
 }
@@ -49,49 +48,28 @@ function create() {
     game.add.sprite(0, 0, "playerImg")
     game.add.sprite(765, 370, "playerImg")
     game.add.audio("score")
-    game.sound.play("score");
 
-    pipes = game.add.group();
 /*
-    for (var rep = 1; rep<=16; rep+=4) {
-/*
+    player = game.add.sprite(x,y,"player")
+        player.x = 100;
+        player.y = 200;
+*/
 
- */
-        var gap = Math.floor(Math.random()*6)+1;
-
-        for (var count = 0; count < gap; count += 1) {
-
-            add_pipe_part(500, 50 * count, "pipe")
-        }
-        for (var count = gap + 3; count <= 8; count++) {
-            add_pipe_part(500, 50 * count, "pipe")
-        }
-
-    /*
-        player = game.add.sprite(x,y,"player")
-            player.x = 100;
-            player.y = 200;
-    */
-
-    player=game.add.sprite(50, 200, "player");
+    player=game.add.sprite(790/2, 400/2, "player");
     player.anchor.setTo(0.5,0.5);
 
     game.physics.arcade.enable(player);
     player.checkWorldBounds = true;
 
     player.body.velocity.y = -100;
-    /*
     player.body.velocity.x= 100;
-    /*
-
-     */
-    player.body.gravity.y = 400;
+    player.body.gravity.y = 200;
 
     game.input.onDown.add(clickHandler)
 
     game.input.keyboard
         .addKey(Phaser.Keyboard.SPACEBAR)
-        .onDown.add(player_jump)
+        .onDown.add(spaceHandler)
 
     var rightkey = game.input.keyboard
         .addKey(Phaser.Keyboard.RIGHT)
@@ -110,8 +88,20 @@ function create() {
         .onDown.add(moveDown);
 
 
-game.time.events
-    .loop(1.5*Phaser.Timer.SECOND, generate_pipes)
+
+for (var rep = 0; rep<=16; rep+=3) {
+
+    var gap = Math.floor(Math.random()*6)+1;
+
+    for (var count = 0; count < gap; count += 1) {
+
+        game.add.sprite(50 * rep, 50 * count, "pipe")
+    }
+    for (var count = gap + 2; count <= 8; count++) {
+        game.add.sprite(50 * rep, 50 * count, "pipe")
+    }
+}
+
 
 
 }
@@ -137,40 +127,16 @@ function clickHandler (event) {
     score = score + 1;
 }
 
-function player_jump () {
-    player.body.velocity.y = -200;
+function spaceHandler () {
+    game.sound.play("score");
+    game.add.sprite(Math.random()*790,Math.random()*400,"playah");
 }
 
-function add_pipe_part(x,y, pipe_part) {
-    var pipe = pipes.create(x,y, pipe_part);
-    game.physics.arcade.enable(pipe)
-    pipe.body.velocity.x = -200
-}
 
-function generate_pipes() {
-    var gap = Math.floor(Math.random()*6)+1;
-
-    for (var count = 0; count < gap; count += 1) {
-
-        add_pipe_part(900, 50 * count, "pipe")
-    }
-    for (var count = gap + 3; count <= 8; count++) {
-        add_pipe_part(900, 50 * count, "pipe")
-    }
-
-}
 /*
  * This function updates the scene. It is called for every new frame.
  */
-function game_over(){
-    alert("Game Over");
-    location.reload()}
-
 function update() {
-    game.physics.arcade
-        .overlap(player, pipes, game_over)
-
+    
 }
-
-
 
